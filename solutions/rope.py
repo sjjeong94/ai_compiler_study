e41bb9a2df524fd33f6903946b3c97696ed7e82e
sjjeong94 <sjjeong94@gmail.com>
2024-03-08 03:30:07 +0900

    rope triton kernel with hard constraits

--- solutions/rope.py
+++ solutions/rope.py
@@ -2,6 +2,7 @@ from typing import Union
 
 import torch
 import triton
+import triton.language as tl
 
 
 def _rotate_half(x: torch.Tensor) -> torch.Tensor:
@@ -13,8 +14,7 @@ def _rotate_half(x: torch.Tensor) -> torch.Tensor:
     return torch.cat((-x2, x1), dim=-1)
 
 
-@torch.jit.script
-def rotary_pos_emb_torch(
+def rope_torch(
     t: torch.Tensor,
     freqs: torch.Tensor,
     tensor_format: str = "sbhd",
@@ -72,7 +72,8 @@ def rotary_pos_emb_torch(
     return torch.cat((t, t_pass), dim=-1)
 
 
-def rotary_pos_emb(
+@torch.jit.script
+def rope_torch_jit(
     t: torch.Tensor,
     freqs: torch.Tensor,
 ) -> torch.Tensor:
@@ -93,17 +94,94 @@ def rotary_pos_emb(
     return torch.cat((t, t_pass), dim=-1)
 
 
+@triton.jit
+def rope_kernel(
+    t_ptr,
+    f_ptr,
+    o_ptr,
+    t_s_stride,
+    f_s_stride,
+    o_s_stride,
+    bh,
+    d,
+    BLOCK_SIZE: tl.constexpr,
+):
+    row_idx = tl.program_id(0)
+    t_start_ptr = t_ptr + row_idx * t_s_stride
+    f_start_ptr = f_ptr + row_idx * f_s_stride
+    o_start_ptr = o_ptr + row_idx * o_s_stride
+
+    col_offsets = tl.arange(0, BLOCK_SIZE)
+    f_ptrs = f_start_ptr + col_offsets
+
+    f = tl.load(f_ptrs)
+
+    cos_ = tl.cos(f)
+    sin_ = tl.sin(f)
+
+    d_half = d // 2
+    for i in range(bh):
+        t_ptrs = t_start_ptr + i * BLOCK_SIZE + col_offsets
+        o_ptrs = o_start_ptr + i * BLOCK_SIZE + col_offsets
+        t = tl.load(t_ptrs)
+
+        t0 = tl.load(t_ptrs - d_half, mask=col_offsets >= d_half, other=0.0)
+        t1 = tl.load(t_ptrs + d_half, mask=col_offsets < d_half, other=0.0)
+        t_rotated = t0 - t1
+
+        y = t * cos_ + t_rotated * sin_
+        tl.store(o_ptrs, y)
+
+
+def rope(
+    t: torch.Tensor,
+    freqs: torch.Tensor,
+) -> torch.Tensor:
+    max_seq_len = freqs.shape[0]
+    cur_seq_len = t.shape[0]
+    assert cur_seq_len <= max_seq_len
+    freqs = freqs[:cur_seq_len]
+    rot_dim = freqs.shape[-1]
+    t, t_pass = t[..., :rot_dim], t[..., rot_dim:]
+
+    s, b, h, d = t.shape
+    BLOCK_SIZE = triton.next_power_of_2(d)
+    num_warps = 4
+    if BLOCK_SIZE >= 2048:
+        num_warps = 8
+    if BLOCK_SIZE >= 4096:
+        num_warps = 16
+
+    o = torch.empty_like(t)
+    bh = b * h
+
+    rope_kernel[(s,)](
+        t,
+        freqs,
+        o,
+        t.stride(0),
+        freqs.stride(0),
+        o.stride(0),
+        bh,
+        d,
+        num_warps=num_warps,
+        BLOCK_SIZE=BLOCK_SIZE,
+    )
+
+    return torch.cat((o, t_pass), dim=-1)
+
+
 @triton.testing.perf_report(
     triton.testing.Benchmark(
         x_names=["S"],  # Argument names to use as an x-axis for the plot
         x_vals=[64 * i for i in range(1, 33)],  # Different possible values for `x_name`
         line_arg="provider",  # Argument name whose value corresponds to a different line in the plot
         # Possible values for `line_arg`
-        line_vals=["torch-jit", "triton"],
+        line_vals=["torch", "torch-jit", "triton"],
         # Label name for the lines
-        line_names=["torch-jit", "triton"],
+        line_names=["torch", "torch-jit", "triton"],
         # Line styles
-        styles=[("green", "-"), ("blue", "-")],
+        styles=[("green", "--"), ("green", "-"), ("blue", "-")],
         ylabel="GB/s",  # Label name for the y-axis
         plot_name="rope-performance",  # Name for the plot, used also as a file name for saving the plot.
         args={},
@@ -111,30 +189,36 @@ def rotary_pos_emb(
 )
 def benchmark(S, provider):
     s, b, h, d = S, 16, 12, 256
-    s2, d2 = 2048, 192
+    s2, d2 = 2048, 256
     t = torch.randn([s, b, h, d], device="cuda")
     freqs = torch.randn([s2, 1, 1, d2], device="cuda")
     quantiles = [0.5, 0.2, 0.8]
+    if provider == "torch":
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: rope_torch(t, freqs), quantiles=quantiles)
     if provider == "torch-jit":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: rotary_pos_emb_torch(t, freqs), quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: rope_torch_jit(t, freqs), quantiles=quantiles)
     if provider == "triton":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: rotary_pos_emb(t, freqs), quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: rope(t, freqs), quantiles=quantiles)
     gbps = lambda ms: 2 * t.nelement() * t.element_size() * 1e-9 / (ms * 1e-3)
     return gbps(ms), gbps(max_ms), gbps(min_ms)
 
 
 torch.manual_seed(0)
 s, b, h, d = 32, 16, 12, 256
-s2, d2 = 48, 192
+s2, d2 = 48, 256
 t = torch.randn([s, b, h, d], device="cuda")
 freqs = torch.randn([s2, 1, 1, d2], device="cuda")
+print(t.stride(0))
 
-triton_output = rotary_pos_emb(t, freqs)
-torch_output = rotary_pos_emb_torch(t, freqs)
+triton_output = rope(t, freqs)
+torch_output = rope_torch(t, freqs)
 print(triton_output.shape, torch_output.shape)
 if torch.allclose(triton_output, torch_output, atol=1e-2, rtol=0):
     print("✅ Triton and Torch match")
 else:
     print("❌ Triton and Torch differ")
+    print(triton_output[1, 0, 1, :277])
+    print(torch_output[1, 0, 1, :277])
+    print(torch.sum(torch.abs(triton_output - torch_output) > 1e-2))
 
 benchmark.run(show_plots=True, print_data=True, save_path=".")
